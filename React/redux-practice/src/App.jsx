3dc56bc7f99789c2dc5eda27a0b5a9fc4755486f
DecidedCard <heajun35@gmail.com>
2024-01-25 15:13:36 +0900

    feat: Redux 연습

--- React/redux-practice/src/App.jsx
+++ React/redux-practice/src/App.jsx
@@ -1,13 +1,33 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import { plusOne, minusOne } from "./redux/modules/counter";
 
 function App() {
-  const data = useSelector((state) => {
-    return state;
+  const counter = useSelector((state) => {
+    return state.counter;
   });
 
-  console.log(data);
-  return <div>Redux!</div>;
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div>현재카운트 : {counter.number}</div>
+      <button
+        onClick={() => {
+          dispatch(plusOne());
+        }}
+      >
+        +
+      </button>
+      <button
+        onClick={() => {
+          dispatch(minusOne());
+        }}
+      >
+        -
+      </button>
+    </>
+  );
 }
 
 export default App;
