dd70efa66b303211849c570616983f952df3ac8d
DecidedCard <heajun35@gmail.com>
2024-01-25 17:18:07 +0900

    feat: Redux 연습

--- React/redux-practice/src/App.jsx
+++ React/redux-practice/src/App.jsx
@@ -1,27 +1,42 @@
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
-import { plusOne, minusOne } from "./redux/modules/counter";
+import { PLUSN, MINUSN } from "./redux/modules/counter";
+import { useState } from "react";
 
 function App() {
+  const [number, setNumber] = useState(0);
+
+  // 여기에서 store에 접근하여, counter의 값을 읽어옴
+  // useSelector
   const counter = useSelector((state) => {
     return state.counter;
   });
 
+  // dispatch
   const dispatch = useDispatch();
 
   return (
     <>
       <div>현재카운트 : {counter.number}</div>
+      <div>
+        <input
+          type="number"
+          value={number}
+          onChange={(event) => {
+            setNumber(+event.target.value);
+          }}
+        />
+      </div>
       <button
         onClick={() => {
-          dispatch(plusOne());
+          dispatch(PLUSN(number));
         }}
       >
         +
       </button>
       <button
         onClick={() => {
-          dispatch(minusOne());
+          dispatch(MINUSN(number));
         }}
       >
         -
--- React/redux-practice/src/redux/modules/counter.js
+++ React/redux-practice/src/redux/modules/counter.js
@@ -1,17 +1,20 @@
 // action value
-const PLUS_ONE = "PLUS_ONE";
-const MINUS_ONE = "MINUS_ONE";
+const PLUS_N = "PLUS_N";
+const MIUNS_N = "MIUNS_N";
 
 // action creator : action value를 return하는 함수
-export const plusOne = () => {
+
+export const PLUSN = (payload) => {
   return {
-    type: PLUS_ONE,
+    type: PLUS_N,
+    payload,
   };
 };
 
-export const minusOne = () => {
+export const MINUSN = (payload) => {
   return {
-    type: MINUS_ONE,
+    type: MIUNS_N,
+    payload,
   };
 };
 
@@ -22,16 +25,18 @@ const initialState = {
 
 // 리듀서 : 'state에 변화를 일으키는'함수
 // (1) state를 action의 type에 따라 변경되는 함수
+
 // input : state와 action
+// action 객체하는 것은 action type을 payload 만큼 처리하는 것
 const counter = (state = initialState, action) => {
   switch (action.type) {
-    case PLUS_ONE:
+    case PLUS_N:
       return {
-        number: state.number + 1,
+        number: state.number + action.payload,
       };
-    case MINUS_ONE:
+    case MIUNS_N:
       return {
-        number: state.number - 1,
+        number: state.number - action.payload,
       };
     default:
       return state;
